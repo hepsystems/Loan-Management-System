c22acf4df8d58ddb0314a8088dfe4caf5d0fcc93
hepsystems <140811776+hepsystems@users.noreply.github.com>
2025-12-28 20:21:01 +0200

    Create constants.js

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,25 @@
+export const LOAN_PURPOSES = [
+  'Business Capital',
+  'Education',
+  'Medical Expenses',
+  'Home Improvement',
+  'Agriculture',
+  'Vehicle Purchase',
+  'Personal Use',
+  'Debt Consolidation',
+  'Wedding',
+  'Emergency'
+];
+
+export const LOAN_LIMITS = {
+  MIN_AMOUNT: 1000,
+  MAX_AMOUNT: 1000000,
+  MIN_TERM: 1,
+  MAX_TERM: 60
+};
+
+export const MOBILE_MONEY_PROVIDERS = [
+  'MPamba',
+  'TNM Mpamba', 
+  'Airtel Money'
+];
